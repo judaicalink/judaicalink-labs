aea0cbea18b5aa8a73569accafaf10c664a39932
Kai Eckert <hallo@kaiec.de>
2020-01-24 19:56:27 +0100

    Cleanup and massive speedup by not using SPARQL

--- labs/backend/dataset_loader.py
+++ labs/backend/dataset_loader.py
@@ -7,7 +7,6 @@ import time
 import gzip
 import os
 from . import models
-from . import sparqltools
 from pathlib import Path
 from datetime import datetime
 from django.utils.http import http_date
@@ -32,32 +31,23 @@ def load_rdf_file(url):
     return filename
 
 
-sparql_query = '''
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX gndo: <http://d-nb.info/standards/elementset/gnd#>
-    PREFIX pro: <http://purl.org/hpi/patchr#>
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-    PREFIX edm: <http://www.europeana.eu/schemas/edm/>
-    PREFIX dc: <http://purl.org/dc/elements/1.1/>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX dblp: <http://dblp.org/rdf/schema-2015-01-26#>
-    PREFIX dcterms: <http://purl.org/dc/terms/>
-    PREFIX bibtex: <http://data.bibbase.org/ontology/#>
-    PREFIX jl: <http://data.judaicalink.org/ontology/>
-select distinct ?o ?name ?bd ?dd ?bl ?dl ?abs (group_concat(?alt; SEPARATOR="-") as ?alt2)
- where
-{
-     ?o skos:prefLabel ?name.
-     ?o skos:altLabel ?alt
-     optional {?o jl:birthDate ?bd}
-     optional {?o jl:deathDate ?dd}
-     optional{ ?o jl:birthLocation ?bl }
-     optional {?o jl:deathLocation ?dl}
-     optional {?o jl:hasAbstract ?abs}
-} group by ?o ?name ?bd ?dd ?bl ?dl ?abs
-'''
+
+property2field = {
+        'http://www.w3.org/2004/02/skos/core#prefLabel': 'name',
+        'http://www.w3.org/2004/02/skos/core#altLabel': 'Alternatives',
+        'http://data.judaicalink.org/ontology/birthDate': 'birthDate',
+        'http://data.judaicalink.org/ontology/birthLocation': 'birthLocation',
+        'http://data.judaicalink.org/ontology/deathDate': 'deathDate',
+        'http://data.judaicalink.org/ontology/deathLocation': 'deathLocation',
+        'http://data.judaicalink.org/ontology/hasAbstract': 'Abstract',
+        }
+
+def cleanstring(value, chars):
+    for c in chars:
+        value = value.replace(c, '')
+    value = re.sub( '\s+', ' ', value).strip()
+    return value
+
 
 
 def index_file(filename, task):
@@ -67,41 +57,40 @@ def index_file(filename, task):
     with openfunc(filename, "rt", encoding="utf8") as f:
         g = rdflib.Graph()
         g.parse(f, format='n3')
-        res = g.query(sparql_query)
         es = elasticsearch.Elasticsearch()
-        bulk_body = [] 
-        for row in res:
-            uri = row[0]
-            name = row[1] 
-            birth = row[2]
-            death = row[3]
-            blocation = row[4]
-            dlocation = row[5]
-            abstract = row[6]
-            if abstract:
-                abstract = abstract.replace('"','')
-                abstract = abstract.replace('{','')
-                abstract = abstract.replace('}','')
-                abstract = re.sub( '\s+', ' ', abstract).strip()
-
-            altname = row[7]
-            if altname:
-                altname = altname.replace('{','')
-                altname = altname.replace('}','')
-                altname = altname.replace('"','')
-                altname = altname.replace('.','')
-
-            doc = { 
-                "Name": name,
-                "birthDate": birth,
-                "deathDate": death,
-                "birthLocation": blocation,
-                "deathLocation": dlocation,
-                "Abstract": abstract,
-                "Alternatives": altname
-                }
+        data = {}
+        triples = g.triples((None, None, None))
+        for row in triples:
+            s = str(row[0])
+            p = str(row[1]) 
+            o = (row[2])
+            if p in property2field:
+                f = property2field[p]
+                if s in data:
+                    doc = data[s]
+                    if f in doc:
+                        doc[f].append(o)
+                    else:
+                        doc[f] = [ o ]
+                else:
+                    data[s] = {
+                            f: [ o ]
+                            }
+        bulk_body = []
+        for s in data:
+            doc = {}
+            for f in data[s]:
+                values = data[s][f]
+                if len(values)==0:
+                    continue
+                values = ' - '.join(values)
+                if f == 'Abstract':
+                    values = cleanstring(values, ['"', '{', '}'])
+                if f == 'Alternatives':
+                    values = cleanstring(values, ['"', '{', '}', '.'])
+                doc[f] = values
             index = {
-                    "index": { "_index": "judaicalink", "_id": uri }
+                    "index": { "_index": "judaicalink", "_id": s }
                     }
             bulk_body.append(json.dumps(index))
             bulk_body.append(json.dumps(doc))
