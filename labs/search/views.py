2e3f75029373dc0b8bd9bdbb2e246ae4f2ba3a9f
sarafischer <sf.fischer.fs@gmail.com>
2020-01-24 14:13:00 +0100

    paging works

--- labs/search/views.py
+++ labs/search/views.py
@@ -1,4 +1,5 @@
 import requests
+import math
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -26,11 +27,16 @@ def load(request):
         response = requests.post('http://localhost:9200/judaicalink/doc/_bulk?pretty', data=data, headers=headers)
         return HttpResponse(response)
 
-def search(request, query):
+def search(request, query, page):
     es = Elasticsearch()
+    size = 10
+    current_page = page -1
+
+    if page > 1:
+        current_page = current_page * size
 
     body = {
-        "from" : 0, "size" : 15,
+        "from" : current_page, "size" : size,
         "query" : {
             "query_string": {
                 "query": query,
@@ -72,12 +78,23 @@ def search(request, query):
             if s in d ["highlight"]:
                 d ["source"] [s] = d ["highlight"] [s] [0]
 
+#    print (result)
+    print (current_page)
+
+    total_hits = result ["hits"] ["total"] ["value"]
+    pages = math.ceil (total_hits / size)   #number of needed pages for paging
+        #round up number of pages
 
     context = {
         "result" : result ["hits"] ["hits"],
             #contains full search results from elasticsearch
         "dataset" : dataset,
             #contains id and information from fields
+        "pages" : pages,
+        "total_hits" : total_hits,
+        "range" : range (1, (pages + 1)),
+        "page" : page,
+        "query" : query,
     }
     return render (request, "search/search_result.html", context)
 
