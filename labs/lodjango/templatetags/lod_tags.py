ce41f953f7212629f5c95102331600c1dd75d0b3
Kai Eckert <hallo@kaiec.de>
2020-01-28 22:26:24 +0000

    Add source graph info

--- labs/lodjango/templatetags/lod_tags.py
+++ labs/lodjango/templatetags/lod_tags.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def local(context, uri):
     return uri.replace(context['settings'].DATA_PREFIX, context['settings'].LOCAL_PREFIX)
+
+
+
+@register.simple_tag()
+def last_path(uri):
+    uri = uri[uri.rfind('/')+1:]
+    uri = uri[uri.rfind('#')+1:]
+    return uri
