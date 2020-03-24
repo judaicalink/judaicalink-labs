f860d3c093ace8a129d7113fcdb9216192b020e2
sarafischer <sf.fischer.fs@gmail.com>
2020-03-24 21:54:37 +0100

    search possible via template (search_index.html)

--- labs/search/urls.py
+++ labs/search/urls.py
@@ -6,8 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.search_index, name='search_index'),
     path('load', views.load, name='load'),
+    path('initial_search', views.initial_search, {"page" : 1}, name='initial_search'),
     path('search/<str:query>', views.search, {"page" : 1}, name='search'),
         #sets default for pagenumber to 1
-    path('search/<str:query>/<int:page>', views.search, name='search'),
+    path('search/<str:query>/<int:page>', views.search, {"page" : 1}, name='search'),
         #used if pagenumber is given
 ]
