2bebd2ddf4bb5f2f9f8d2c0cbf59a1e2bde1cc05
Patricia Kaluza <petulacicle@gmail.com>
2020-06-05 05:21:45 +0200

    labs.css, Design.js und root.html aktualisiert

--- labs/search/static/js/Design.js
+++ labs/search/static/js/Design.js
@@ -13,6 +13,20 @@ $(document).ready(function () {
         });
 
 /*------------Sidebar active page highlighting---------------*/
+
+$(document).ready(function ($) {
+    var path = window.location.pathname.split("/").pop();
+
+    if( path == ''){
+        path = ''; /*das ist die Startseite von Labs http://labs.judaicalink.org/!leer!/*/
+    }
+
+    var target = $('#sidebar a[href="'+path+'"]');
+
+    target.addClass('active');
+    });
+
+/*---------------------------------------------------*/
 /*$(document).on('click', 'ul li', function(){
                 $(this).addClass('active').siblings().removeClass('active')
                 })
