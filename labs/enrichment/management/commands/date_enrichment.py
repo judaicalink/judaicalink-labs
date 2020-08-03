e77469131541bd085356028a707b0d6ffcbf1844
sarafischer <sf.fischer.fs@gmail.com>
2020-08-03 15:00:10 +0200

    date_enrichment.py und index_file.py aktualisiert

--- labs/enrichment/management/commands/date_enrichment.py
+++ labs/enrichment/management/commands/date_enrichment.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 import gzip
 import rdflib
 from backend import models, dataset_loader
+import re
 
 class Command(BaseCommand):
     help = 'Create parsed dates from birthDate and deathDate'
@@ -36,22 +37,32 @@ class Command(BaseCommand):
                         if row[1] == jl.birthDate:
                             date_string = str(row[2])
                             # do your parsing magic...
-                            if len(date_string) >= 1 and date_string.strip(" ?").isnumeric():
-                                year = int(date_string.strip(" ?"))
-                                print(year)
+                            if date_string.strip().isnumeric() and len(date_string) <= 4:
+                                year = int(date_string.strip())
+                                #print(year)
                                 # create new triple.
                                 # Check this: https://rdflib.readthedocs.io/en/stable/rdf_terms.html
                                 out.add((row[0], jl.birthYear, rdflib.Literal(year, datatype=rdflib.XSD.integer)))
                             # no magic available? then it is unparsed
-                            elif "-" in date_string and date_string[:4].isnumeric():
-                                #1980-04-01 -> only get 1980
-                                year = int(date_string[:4])
-                                print(year)
-                                out.add((row[0], jl.birthYear, rdflib.Literal(year, datatype=rdflib.XSD.integer)))
-                            elif "." in date_string and date_string[4:].isnumeric():
-                                #01.04.1980 -> only get 1980
-                                year = int(date_string[4:])
-                                print(year)
+                            # elif "-" in date_string and date_string[:4].isnumeric():
+                            #     #1980-04-01 -> only get 1980
+                            #     year = int(date_string[:4])
+                            #     print(year)
+                            #     out.add((row[0], jl.birthYear, rdflib.Literal(year, datatype=rdflib.XSD.integer)))
+                            # elif "." in date_string and date_string.strip()[-4:].isnumeric():
+                            #     #01.04.1980 -> only get 1980
+                            #     year = int(date_string.strip()[-4:])
+                            #     print(year)
+                            #     out.add((row[0], jl.birthYear, rdflib.Literal(year, datatype=rdflib.XSD.integer)))
+                            elif len (date_string) >= 4:
+                                numbers = re.findall('[0-9+]', date_string)
+                                #removes all characters except for numbers
+                                #https://docs.python.org/3/library/re.html
+                                for i in numbers:
+                                    if len (i) == 4:
+                                        #gets first year if two years are in date_string
+                                        year = int(i)
+                                #print(year)
                                 out.add((row[0], jl.birthYear, rdflib.Literal(year, datatype=rdflib.XSD.integer)))
                             else:
                                 unparsed.add(date_string)
---
+++ labs/enrichment/management/commands/index_file.py
@@ -0,0 +1,84 @@
+import json
+import re
+import elasticsearch
+import rdflib
+import gzip
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+property2field = {
+        'http://www.w3.org/2004/02/skos/core#prefLabel': 'name',
+        'http://www.w3.org/2004/02/skos/core#altLabel': 'Alternatives',
+        'http://data.judaicalink.org/ontology/birthDate': 'birthDate',
+        'http://data.judaicalink.org/ontology/birthYear': 'birthYear',
+        'http://data.judaicalink.org/ontology/birthLocation': 'birthLocation',
+        'http://data.judaicalink.org/ontology/deathDate': 'deathDate',
+        'http://data.judaicalink.org/ontology/deathYear': 'deathYear',
+        'http://data.judaicalink.org/ontology/deathLocation': 'deathLocation',
+        'http://data.judaicalink.org/ontology/hasAbstract': 'Abstract',
+        }
+
+def cleanstring(value, chars):
+    for c in chars:
+        value = value.replace(c, '')
+    value = re.sub( '\s+', ' ', value).strip()
+    return value
+
+class Command(BaseCommand):
+    help = 'index data from a file in elasticsearch'
+
+    def add_arguments(self, filepath):
+        filepath.add_argument('filepath', type=str, help='Filepath to file that needs to be indexed')
+
+    def handle(self, *args, **kwargs):
+        filepath = kwargs['filepath']
+        openfunc = open
+        if filepath.endswith(".gz"):
+            openfunc = gzip.open
+        with openfunc(filepath, "rt", encoding="utf8") as f:
+            g = rdflib.Graph()
+            g.parse(f, format='n3')
+            es = elasticsearch.Elasticsearch()
+            data = {}
+            triples = g.triples((None, None, None))
+            for row in triples:
+                s = str(row[0])
+                p = str(row[1])
+                o = (row[2])
+                if p in property2field:
+                    f = property2field[p]
+                    if s in data:
+                        doc = data[s]
+                        if f in doc:
+                            doc[f].append(o)
+                        else:
+                            doc[f] = [ o ]
+                    else:
+                        data[s] = {
+                                f: [ o ]
+                                }
+            bulk_body = []
+            for s in data:
+                doc = {}
+                for f in data[s]:
+                    values = data[s][f]
+                    if len(values)==0:
+                        continue
+                    values = ' - '.join(values)
+                    if f == 'Abstract':
+                        values = cleanstring(values, ['"', '{', '}'])
+                    if f == 'Alternatives':
+                        values = cleanstring(values, ['"', '{', '}', '.'])
+                    doc[f] = values
+                #indexing slug
+                dataslug = s
+                dataslug = dataslug.replace ("http://data.judaicalink.org/data/", "").split ("/")[0]
+                doc ["dataslug"] = dataslug
+                index = {
+                        "update": { "_index": settings.JUDAICALINK_INDEX, "_id": s }
+                        }
+                bulk_body.append(json.dumps(index))
+                bulk_body.append(json.dumps({"doc": doc, "doc_as_upsert": True}))
+            if len(bulk_body)>0:
+                print ('indexing successful!')
+                es.bulk('\n'.join(bulk_body), request_timeout=60)
