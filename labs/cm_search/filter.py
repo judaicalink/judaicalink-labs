d5162b0bd3db208956f09c063c8b97421bac942d
sarafischer <sf.fischer.fs@gmail.com>
2022-01-08 03:00:55 +0100

    Update filter.py and filter.html

--- labs/cm_search/filter.py
+++ labs/cm_search/filter.py
@@ -1,10 +1,14 @@
 # function that returns a list of all distinct journals in result
 # result is a list of items that match a previous search
-def get_distinct_journals(result):
-    journals = []
-    for entry in result:
-        if entry.journal not in journals:
-            journals.append(entry.journal)
+def get_distinct_journals(result, checked_items):
+    unchecked_journals = []
+    for formatted_doc in result:
+        if formatted_doc["j_title"] not in unchecked_journals and formatted_doc["j_title"] not in checked_items:
+            unchecked_journals.append(formatted_doc["j_title"])
+    journals = {
+        "checked": checked_items,
+        "unchecked": unchecked_journals
+        }
     return journals
 
 
@@ -12,18 +16,18 @@ def get_distinct_journals(result):
 # result is a list of items that match a previous search
 def get_distinct_languages(result):
     languages = []
-    for entry in result:
-        if entry.language not in languages:
-            languages.append(entry.language)
+    for formatted_doc in result:
+        if formatted_doc["lang"] not in languages:
+            languages.append(formatted_doc["lang"])
     return languages
 
 
 # function that returns a list of all distinct years items were issued in in result
 # result is a list of items that match a previous search
-    # might need changes to only get the year out of items with date of issue like '1919-01-01'
+    # works only for dates of issue like '1919-01-01' -> gets '1919'
 def get_distinct_years(result):
     years = []
-    for entry in result:
-        if entry.year not in years:
-            years.append(entry.year)
+    for formatted_doc in result:
+        if formatted_doc["dateIssued"][:4] not in years:
+            years.append(formatted_doc["dateIssued"][:4])
     return years
