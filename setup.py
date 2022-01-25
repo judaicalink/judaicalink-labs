50dd620d1225221456e092b93904996f0c8b7611
bsesic <Benjamin-777@gmx.de>
2022-01-25 14:27:58 +0100

    setup.py, setup.cfg and pyproject.toml added.

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='judaicalink-labs',
+      version='0.0.1',
+      packages=find_packages(),
+      scripts=['manage.py'])
+
