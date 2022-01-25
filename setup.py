b82c92e20917be5c25d7a6879251761a01dee3b3
bsesic <Benjamin-777@gmx.de>
2022-01-25 14:32:24 +0100

    Path to manage.py and Version changed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='judaicalink-labs',
-      version='0.0.1',
+      version='0.1',
       packages=find_packages(),
-      scripts=['manage.py'])
+      scripts=['labs/manage.py'])
 
