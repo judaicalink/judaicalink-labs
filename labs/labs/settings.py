076578dc59930faa79007f2b5be2eb205a6fe5ae
bsesic <Benjamin-777@gmx.de>
2022-02-02 14:55:35 +0100

    settings merged.

--- labs/labs/settings.py
+++ labs/labs/settings.py
@@ -10,19 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import logging
+
+import environ
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# SECURITY WARNING: don't run with debug turned on in production!
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# False if not in os.environ because of casting above
+DEBUG = env('DEBUG')
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sx(-a*38sn4d_tspf_fn7%q^w=1t+w^r9g9(mz_7nu#9oaej1_'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = [
     'labs.judaicalink.org',
@@ -48,8 +61,9 @@ INSTALLED_APPS = [
     'dashboard',
     'data',
     'crispy_forms',
-    'captcha',
+    'hcaptcha',
     'active_link',
+    'environ',
 ]
 
 MIDDLEWARE = [
@@ -68,8 +82,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
-        ],
+             os.path.join(BASE_DIR, 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -88,10 +102,30 @@ WSGI_APPLICATION = 'labs.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # read os.environ['DATABASE_URL'] and raises
+    # ImproperlyConfigured exception if not found
+    #
+    # The db() method is an alias for db_url().
+    'default': env.db(),
+
+    # read os.environ['SQLITE_URL']
+    'extra': env.db_url(
+        'SQLITE_URL',
+        default='sqlite:///db.sqlite3'
+    )
+}
+
+# Cache
+
+CACHES = {
+    # Read os.environ['CACHE_URL'] and raises
+    # ImproperlyConfigured exception if not found.
+    #
+    # The cache() method is an alias for cache_url().
+    'default': env.cache(),
+
+    # read os.environ['REDIS_URL']
+    'redis': env.cache_url('REDIS_URL')
 }
 
 # Password validation
@@ -129,20 +163,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Crispy form
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-
-EMAIL_TO = 'benjamin.schnabel@hdm-stuttgart.de'
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_TO = env('EMAIL_TO')
 
 # Channels
 ASGI_APPLICATION = "labs.routing.application"
@@ -154,27 +186,38 @@ CHANNEL_LAYERS = {
 }
 
 # labs settings
-LABS_ROOT = 'http://localhost:8000/'
-LABS_GIT_WEBROOT = "https://github.com/wisslab/judaicalink-labs/blob/master/labs/"
-LABS_DUMPS_WEBROOT = "http://data.judaicalink.org/dumps/"
-LABS_DUMPS_LOCAL = "dumps/"
+LABS_ROOT = env('LABS_ROOT') if env('LABS_ROOT') is not None else 'http://localhost:8000'
+LABS_GIT_WEBROOT = env('LABS_GIT_WEBROOT') if env('LABS_GIT_WEBROOT') is not None else "https://github.com/wisslab/judaicalink-labs/blob/master/labs/"
+LABS_DUMPS_WEBROOT = env('LABS_DUMPS_WEBROOT') if env('LABS_DUMPS_WEBROOT') is not None else "http://data.judaicalink.org/dumps/"
+LABS_DUMPS_LOCAL = env('LABS_DUMPS_LOCAL') if env('LABS_DUMPS_LOCAL') is not None else "dumps/"
 
 # Fuseki
-FUSEKI_SERVER = "http://localhost:3030/"
-FUSEKI_STORAGE = "."
+FUSEKI_SERVER = env('FUSEKI_SERVER') if env('FUSEKI_SERVER') is not None else "http://localhost:3030"
+FUSEKI_STORAGE = env('FUSEKI_STORAGE') if env('FUSEKI_STORAGE') is not None else "."
+
 
 # Elasticsearch
-ELASTICSEARCH_SERVER = "https://localhost:9200/" if os.environ.get('ELASTICSEARCH_SERVER') is None else os.environ.get('ELASTICSEARCH_SERVER')
-
+ELASTICSEARCH_SERVER = "https://localhost:9200/" if env('ELASTICSEARCH_SERVER') is None else env('ELASTICSEARCH_SERVER')
 ELASTICSEARCH_STORAGE = "/var/lib/elasticsearch"
-JUDAICALINK_INDEX = "judaicalink"
-COMPACT_MEMORY_INDEX = "cm"
-COMPACT_MEMORY_META_INDEX = "cm_meta"
-
 ELASTICSEARCH_SSL_ENABLED = False if ELASTICSEARCH_SERVER.startswith("http://") else True
+JUDAICALINK_INDEX = env('JUDAICALINK_INDEX') if env('JUDAICALINK_INDEX') is not None else "judaicalink"
+COMPACT_MEMORY_INDEX = env('COMPACT_MEMORY_INDEX') if env('COMPACT_MEMORY_INDEX') is not None else "cm"
+COMPACT_MEMORY_META_INDEX = env('COMPACT_MEMORY_META_INDEX') if env('COMPACT_MEMORY_META_INDEX') is not None else "cm_meta"
+
+# HCaptcha
+HCAPTCHA_SITEKEY = env('HCAPTCHA_SITEKEY')
+HCAPTCHA_SECRET = env('HCAPTCHA_SECRET')
+
+HCAPTCHA_DEFAULT_CONFIG = {
+    'onload': 'name_of_js_function',
+    'render': 'explicit',
+    'theme': 'light',  # do not use data- prefix
+    'size': 'normal',  # do not use data- prefix
+}
+
 
 # if the settings in the .env contain the ELASTICSEARCH_SERVER_CERT_PATH use it, else throw an error
-if ELASTICSEARCH_SSL_ENABLED and 'ELASTICSEARCH_SERVER_CERT_PATH' not in os.environ:
+if ELASTICSEARCH_SSL_ENABLED and env('ELASTICSEARCH_SERVER_CERT_PATH') is not None:
     ELASTICSEARCH_SERVER_CERT_PATH = os.environ['ELASTICSEARCH_SERVER_CERT_PATH']
 else:
     logging.ERROR("ELASTICSEARCH_SERVER_CERT_PATH not set in .env file")
