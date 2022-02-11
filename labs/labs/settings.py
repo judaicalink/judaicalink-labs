de62aac8b0d22bd505c50a49494fa54629334233
bsesic <Benjamin-777@gmx.de>
2022-02-11 17:27:52 +0100

    Debug Toolbar in settings.py

--- labs/labs/settings.py
+++ labs/labs/settings.py
@@ -218,6 +218,23 @@ HCAPTCHA_DEFAULT_CONFIG = {
     'size': 'normal',  # do not use data- prefix
 }
 
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.history.HistoryPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
+]
+RENDER_PANELS = True
 
 GEONAMES_API_USER = "" # Configure in localsettings.py
 
