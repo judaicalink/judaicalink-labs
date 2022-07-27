cd029f8768d64c9b0c93fefa8481db0355b85a06
bsesic <Benjamin-777@gmx.de>
2022-07-27 23:34:21 +0200

    autocomplete included.

--- labs/src/js/app.js
+++ labs/src/js/app.js
@@ -13,19 +13,31 @@ import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
 require('./activePageHighlighting.js'); // import active page highlighting javascript files
-//import('./autocomplete.js'); // import auto complete for cm_e_search
-import Vue from 'vue';
+import('./autocomplete.js'); // import auto complete for cm_e_search
 
 require('bootstrap');
 require('malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js')
 require('./design.js')
 
 
-import { createApp } from 'vue'
+//import { createApp } from 'vue'
 // import the root component App from a single-file component.
-import App from '../vue/search.vue'
+//import Vue from 'vue';
+//import SearchForm from './SearchForm.vue';
+//Vue.config.productionTip = false;
+//new Vue({
+//  render: h => h(SearchForm),
+//}).$mount('#searchForm');
 
-const app = createApp(App)
-app.mount('#app')
+//const app = createApp(App)
+//app.mount('#app')
 
-//require('../vue/search.vue')
+import {Vue} from 'vue';
+import search  from "../vue/search.vue";
+
+new Vue({
+    el: '#app',
+    components: {
+        search
+    }
+} ).$mount('#app');
