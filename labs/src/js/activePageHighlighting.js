ae00cbd2f0a93c1bcbca9250857ecbaec4c1893c
sarafischer <sf.fischer.fs@gmail.com>
2022-10-14 17:46:22 +0200

    Update styles.css, styles.css.gz, and 10 more files...

--- labs/src/js/activePageHighlighting.js
+++ labs/src/js/activePageHighlighting.js
@@ -1,7 +1,14 @@
 pathName = window.location.pathname.trim();
 page = pathName.split("/").filter(function (e) {return e != null;});
 
+console.log (pathName)
+console.log (page)
+
 if (page[1] != null) {
     //console.log('Active page: ' + page[1]);
+    console.log("a[href='/" + page[1] + "']");
     $("a[href='/" + page[1] + "']").addClass("active");
+}
+else {
+    console.log('no element available');
 }
