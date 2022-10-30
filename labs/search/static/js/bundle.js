4db842470b42fd70181a7df080921ae6d3a718c1
sarafischer <sf.fischer.fs@gmail.com>
2022-10-30 16:35:57 +0100

    fixed highlighting of current page in navbar

--- labs/search/static/js/bundle.js
+++ labs/search/static/js/bundle.js
@@ -20759,6 +20759,7 @@ console.log(page);
 if (page[1] != null) {
   //console.log('Active page: ' + page[1]);
   console.log("a[href='/" + page[1] + "']");
+  console.log(page[1]);
   $("a[href='/" + page[1] + "']").addClass("active");
 } else {
   console.log('no element available');
