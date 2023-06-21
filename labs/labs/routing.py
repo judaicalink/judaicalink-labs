89f158c2ec5f9876c1a4058d01bc4065d234490d
Benjamin Schnabel <Benjamin-777@gmx.de>
2023-06-21 10:32:05 +0200

    routing for channels 4

--- labs/labs/routing.py
+++ labs/labs/routing.py
@@ -1,9 +1,13 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import backend.routing
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             backend.routing.websocket_urlpatterns
