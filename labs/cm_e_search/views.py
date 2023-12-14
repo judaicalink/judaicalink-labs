9e410640a01a538df8c7ffb196090c537e6cf4bb
Benjamin Schnabel <Benjamin-777@gmx.de>
2023-12-14 14:35:10 +0100

    names in query

--- labs/cm_e_search/views.py
+++ labs/cm_e_search/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import render
 from django.http import HttpResponse
 import pysolr
@@ -54,12 +56,14 @@ def create_graph_visualization():
 def result(request):
 
 	names = get_names() # searches for all names in cm_entity_names
+	logging.info(names)
 
 	query = request.GET.get('query')
 
 	solr = pysolr.Solr(settings.SOLR_SERVER + 'cm_entities', always_commit=True, timeout=10, auth=(settings.SOLR_USER, settings.SOLR_PASSWORD))
 
 	res = solr.search(query, index='cm_entities', rows=10000)
+	logging.info(res)
 
 	result = []
 	for doc in res:
