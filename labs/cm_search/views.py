5d333c5af54a30d530fc3fa86ace0d334f5872b2
Benjamin Schnabel <Benjamin-777@gmx.de>
2024-01-22 16:24:27 +0100

    cm debugging

--- labs/cm_search/views.py
+++ labs/cm_search/views.py
@@ -37,7 +37,10 @@ def result(request):
     highlight_fields = ["page", "text", "dateIssued", "j_title", "volume", "heft", "aufsatz"]
     fields = ["page", "text", "dateIssued", "j_title", "volume", "heft", "aufsatz"]
     # create a dict from the fields and add the query
-    solr_query = {field: query for field in fields}
+    #solr_query = {field: query for field in fields}
+    # create a list for the fields that should be searched and add the query
+    solr_query = [field + ":" + query for field in fields]
+
     print("Solr query: ", solr_query)
 
     # FIXME: query does not work
