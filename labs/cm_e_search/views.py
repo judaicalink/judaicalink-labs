0638b26740b4ef59e6688f8d9e440a206c62c3fd
Benjamin Schnabel <Benjamin-777@gmx.de>
2024-01-26 12:00:26 +0100

    occurrences debugging

--- labs/cm_e_search/views.py
+++ labs/cm_e_search/views.py
@@ -138,35 +138,37 @@ def result(request):
         else:
             doc['related_entities'] = []
 
-        # rebuild the occurences
-        print("Journal occs: ", doc['journal_occs.j_name'])
+        # check for journal occurences
+        if 'journal_occs.j_name' in doc:
+            # rebuild the occurences
+            print("Journal occs: ", doc['journal_occs.j_name'])
 
-        doc['occurrences'] = []
+            doc['occurrences'] = []
 
-        occurrence = {}
-        for index in range(0, len(doc['journal_occs.j_name'])-1):
+            occurrence = {}
+            for index in range(0, len(doc['journal_occs.j_name'])-1):
 
-            occurrence['j_name'] = doc['journal_occs.j_name'][index]
-            occurrence['j_id'] = doc['journal_occs.j_id'][index]
-            occurrence['first'] = doc['journal_occs.first'][index]
-            occurrence['last'] = doc['journal_occs.last'][index]
-            occurrence['mentions'] = {}
-            occurrence['mentions']['p_id'] = doc['journal_occs.mentions.p_id'][index]
-            occurrence['mentions']['spot'] = doc['journal_occs.mentions.spot'][index]
-            occurrence['mentions']['start'] = doc['journal_occs.mentions.start'][index]
-            occurrence['mentions']['end'] = doc['journal_occs.mentions.end'][index]
-            occurrence['mentions']['p_link'] = doc['journal_occs.mentions.p_link'][index]
-            occurrence['mentions']['date'] = doc['journal_occs.mentions.date'][index]
-            occurrence['mentions']['year'] = doc['journal_occs.mentions.year'][index]
-            print("Occurence: ", occurrence)
+                occurrence['j_name'] = doc['journal_occs.j_name'][index]
+                occurrence['j_id'] = doc['journal_occs.j_id'][index]
+                occurrence['first'] = doc['journal_occs.first'][index]
+                occurrence['last'] = doc['journal_occs.last'][index]
+                occurrence['mentions'] = {}
+                occurrence['mentions']['p_id'] = doc['journal_occs.mentions.p_id'][index]
+                occurrence['mentions']['spot'] = doc['journal_occs.mentions.spot'][index]
+                occurrence['mentions']['start'] = doc['journal_occs.mentions.start'][index]
+                occurrence['mentions']['end'] = doc['journal_occs.mentions.end'][index]
+                occurrence['mentions']['p_link'] = doc['journal_occs.mentions.p_link'][index]
+                occurrence['mentions']['date'] = doc['journal_occs.mentions.date'][index]
+                occurrence['mentions']['year'] = doc['journal_occs.mentions.year'][index]
+                print("Occurence: ", occurrence)
 
-            # add the data to the results
-            #results.append(doc)
-            doc['occurrences'].append(occurrence)
-            
-    print("Occurences: ", doc['occurrences'])
+                # add the data to the results
+                #results.append(doc)
+                doc['occurrences'].append(occurrence)
 
-    #print("Results: ", results)
+        print("Occurences: ", doc['occurrences'])
+
+    print("Results: ", results)
     context = {
         "results": results,
         "data": json.dumps(names)
