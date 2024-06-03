95cb9781d5d72b2201fd0164e9848063687e3388
bsesic <Benjamin-777@gmx.de>
2024-06-03 10:50:32 +0200

    list flattend

--- labs/search/views.py
+++ labs/search/views.py
@@ -427,7 +427,7 @@ def process_query(query_dic, page, alert):
             for field in highlight_fields:
                 if field in highlighting[doc_id]:
                     # Replace the original field value with the flattened highlighted value
-                    doc[field] = " ".join(highlighting[doc_id][field])
+                    doc[field] = highlighting[doc_id][field][0]
 
     # FIXME: add the source to the data
     """
