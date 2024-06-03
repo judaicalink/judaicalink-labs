639c4a577aa09692c9dab8d4e5dd6dbca0e11f09
bsesic <Benjamin-777@gmx.de>
2024-06-03 12:05:16 +0200

    capitalized and ordered field

--- labs/search/views.py
+++ labs/search/views.py
@@ -433,13 +433,9 @@ def process_query(query_dic, page, alert):
 
     for doc in data:
         capitalized_doc = {key.capitalize(): value for key, value in doc.items()}
+        capitalized_doc.pop('Id', None)
         doc.clear()
         doc.update(capitalized_doc)
-
-    # Remove the 'id' key from each document
-    for doc in data:
-        doc.pop('Id', None)
-
 
     total_hits = result.hits
     pages = []
