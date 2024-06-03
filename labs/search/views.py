7d8c059543b2ba2a6bfd5a64928df3da375cf683
bsesic <Benjamin-777@gmx.de>
2024-06-03 11:07:52 +0200

    highlight in link

--- labs/search/views.py
+++ labs/search/views.py
@@ -426,9 +426,8 @@ def process_query(query_dic, page, alert):
         if doc_id in highlighting:
             for field in highlight_fields:
                 if field in highlighting[doc_id]:
-                    if not field['link']:
-                        # Replace the original field value with the flattened highlighted value
-                        doc[field] = "".join(highlighting[doc_id][field])
+                    # Replace the original field value with the flattened highlighted value
+                    doc[field] = "".join(highlighting[doc_id][field])
 
     # FIXME: add the source to the data
     """
