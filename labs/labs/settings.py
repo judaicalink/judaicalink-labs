ff468234a863cc8f2a5834f634b544327f30e4f4
bsesic <Benjamin-777@gmx.de>
2024-11-12 14:49:05 +0100

    settings updated

--- labs/labs/settings.py
+++ labs/labs/settings.py
@@ -91,13 +91,14 @@ ROOT_URLCONF = 'labs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+             os.path.join(BASE_DIR, 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.template.context_processors.settings',  # Required to access settings in templates
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
