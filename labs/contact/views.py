4a9a4e4c7e693bb72cbc862e83dd36bfa6a02a4a
bsesic <Benjamin-777@gmx.de>
2024-11-13 11:50:13 +0100

    views updated

--- labs/contact/views.py
+++ labs/contact/views.py
@@ -20,62 +20,64 @@ logger = logging.getLogger(__name__)
 #@cache_page(CACHE_TTL)
 def index(request):
     error_message = '' # Default error message
-    logger.info('Search index page')
-    logger.debug('DEBUG: %s', settings.DEBUG)
 
     if request.POST:
         logger.info('POST request')
         # Process the form
         form = ContactForm(request.POST)
-        if form.is_valid():
-            # Send mail
-            logger.info('DEBUG: %s', settings.DEBUG)
-            logger.info('Request: %s', request)
-            logger.info('Form: %s', form)
-            name = request.POST.get('name')
 
-            subject = "New request from Judaicalink.org."
-            from_email = request.POST.get('email')
-            from_name_email = '{} <{}>'.format(name, from_email)
-            message = request.POST.get('message')
+        # Send mail
+        logger.info('DEBUG: %s', settings.DEBUG)
+        logger.info('Request: %s', request)
+        logger.info('Form: %s', form)
+        name = request.POST.get('name')
 
-            # For debugging
-            logger.debug('Name: %s', name)
-            logger.debug('Email: %s', from_email)
-            logger.debug('Message: %s', message)
-            logger.debug('Subject: %s', subject)
+        subject = "New request from Judaicalink.org."
+        from_email = request.POST.get('email')
+        from_name_email = '{} <{}>'.format(name, from_email)
+        message = request.POST.get('message')
 
-            # TODO: Add hCaptcha verification
-            captcha = request.POST.get('h-captcha-response')
-            logger.debug('Captcha: %s', captcha)
+        # For debugging
+        logger.debug('Name: %s', name)
+        logger.debug('Email: %s', from_email)
+        logger.debug('Message: %s', message)
+        logger.debug('Subject: %s', subject)
 
-            # if the message, the from_email, the name and the captcha are not empty
-            if message and from_email and name and captcha:
-                logger.info('Sending mail')
-                try:
-                    # Send mail
-                    # TODO: change mailing system
-                    #send_mail(subject=subject, message=content, from_email=from_name_mail,  recipient_list=[settings.EMAIL_TO], fail_silently=False, html_message=False, auth_user=settings.EMAIL_HOST_USER, auth_password=settings.EMAIL_HOST_PASSWORD)
-                    send_mail(subject, message=message, from_email=from_name_email, recipient_list=["b.schnabel@hs-mannheim.de"], fail_silently=False)
+        # TODO: Add hCaptcha verification
+        captcha = request.POST.get('h-captcha-response')
+        logger.debug('Captcha: %s', captcha)
 
-                except BadHeaderError:
-                    error_message = 'Invalid header found.'
-                    return render(request, 'contact/contact.html', {'form': form, 'error_message': error_message})
+        # if the message, the from_email, the name and the captcha are not empty
+        if message and from_email and name and captcha:
+            logger.info('Sending mail')
+            try:
+                # Send mail
+                # TODO: change mailing system
+                #send_mail(subject=subject, message=content, from_email=from_name_mail,  recipient_list=[settings.EMAIL_TO], fail_silently=False, html_message=False, auth_user=settings.EMAIL_HOST_USER, auth_password=settings.EMAIL_HOST_PASSWORD)
+                send_mail(subject, message=message, from_email=from_name_email, recipient_list=["b.schnabel@hs-mannheim.de"], fail_silently=False)
 
-                except Exception as e:
-                    # email not sent
-                    logger.debug('Error, Email not sent', e)
-                    error_message = f'Email not sent. Please try again. {e}'
-                    return render(request, 'contact/contact.html', {'form': form, 'error_message': error_message})
+            except BadHeaderError:
+                error_message = 'Invalid header found.'
+                return render(request, 'contact/contact.html', {'form': form, 'error_message': error_message})
 
-                logger.info('Mail sent')
-                return HttpResponseRedirect(reverse('contact:sent'))
+            except Exception as e:
+                # email not sent
+                logger.debug('Error, Email not sent', e)
+                error_message = f'Email not sent. Please try again. {e}'
+                return render(request, 'contact/contact.html', {'form': form, 'error_message': error_message})
 
-            else:
-                error_message = 'Form is not valid.'
+            logger.info('Mail sent')
+            return HttpResponseRedirect(reverse('contact:sent'))
 
         else:
             error_message = 'Form is not valid.'
+            form.add_error('message', 'Please fill in the message.')
+            form.add_error('email', 'Please fill in the email.')
+            form.add_error('Name', 'Please fill in the name.')
+            form.add_error('captcha', 'Please fill in the captcha.')
+            form.add_error(None, 'Please fill in all fields.')
+
+
 
     else:
         # Render the default form
