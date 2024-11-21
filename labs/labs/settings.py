b93cb0e040fc0f10ab24c8062c9364fd40fe26af
bsesic <Benjamin-777@gmx.de>
2024-11-21 20:46:27 +0100

    autocomplete updated

--- labs/labs/settings.py
+++ labs/labs/settings.py
@@ -82,13 +82,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'labs.middleware.SuppressVariableDoesNotExistMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
