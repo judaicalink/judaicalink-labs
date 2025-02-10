70cd63b26aa8af6f2c7f28680e1493be4d98ae39
Benjamin Schnabel <Benjamin-777@gmx.de>
2025-02-10 14:59:12 +0100

    load function removed

--- labs/search/urls.py
+++ labs/search/urls.py
@@ -18,7 +18,6 @@ app_name = 'search'
 urlpatterns = [
     path('judaicalink_search_index', views.search_index, name='judaicalink_search_index'),
     path('all_search_nav', views.all_search_nav, name='all_search_nav'),
-    path('load', views.load, name='load'),
     path('search', views.search, name='search'),
     path('advanced', views.advanced, name='advanced'),
     # the sitemap
