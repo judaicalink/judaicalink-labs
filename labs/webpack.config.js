69be5e36c9ce24773dc73d49d558da811c449b47
Benjamin Schnabel <Benjamin-777@gmx.de>
2025-06-26 14:29:45 +0200

    hashing and splitting removed

--- labs/webpack.config.js
+++ labs/webpack.config.js
@@ -1,129 +1,96 @@
-require('dotenv').config();
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-const {VueLoaderPlugin} = require('vue-loader');
+const { VueLoaderPlugin } = require('vue-loader');
 const webpack = require('webpack');
 const BundleTracker = require('webpack-bundle-tracker');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 module.exports = {
-    entry: {
-        app: './src/js/app.js',
-        bootstrap: './src/js/bootstrap.js',
-        styles: './src/scss/app.scss',
-        autocomplete: './src/js/autocomplete.js', // for autocomplete
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'), // Webpack compiles into `build/`
-        filename: 'js/[name].[contenthash].js',
-        chunkFilename: 'js/[name].[contenthash].js', // Dynamic chunks in static/js/
-        publicPath: '/static/', // Django serves files from /static/
-        sourceMapFilename: 'js/[name].[contenthash].js.map',
-    },
-    mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-    devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : 'source-map',
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                use: ['source-map-loader'],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'img/[name].[contenthash][ext]', // Output images to static/img/
-                },
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name].[contenthash][ext]', // Output fonts to static/fonts/
-                },
-            },
-        ],
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            __VUE_OPTIONS_API__: true, // or false if you're not using `options` API
-            __VUE_PROD_DEVTOOLS__: false,
-            __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false, // usually fine
-        }),
-        new BundleTracker({path: __dirname, filename: 'webpack-stats.json'}),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash].css', // Output CSS to static/css/
-        }),
-        new VueLoaderPlugin(),
-        new CompressionPlugin({
-            test: /\.(js|css|html|svg)$/,
-            algorithm: 'gzip',
-            threshold: 10240,
-            minRatio: 0.8,
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            'window.$': 'jquery',
-            Popper: ['@popperjs/core', 'default'],
-        }),
-        new CopyWebpackPlugin({
-            patterns: [{
-                from: path.resolve(__dirname, 'node_modules/@popperjs/core/dist/umd/popper.min.js.map'),
-                to: 'js'
-            },
-                {
-                    from: path.resolve(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.min.js.map'),
-                    to: 'js'
-                }
-            ]
-        }),
+  entry: {
+    app: './src/js/app.js',
+    bootstrap: './src/js/bootstrap.js',
+    styles: './src/scss/app.scss',
+    autocomplete: './src/js/autocomplete.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'js/[name].js',
+    publicPath: '/static/',
+    sourceMapFilename: 'js/[file].map',
+  },
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+  devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : 'source-map',
+  module: {
+    rules: [
+      { enforce: 'pre', test: /\.js$/, use: ['source-map-loader'] },
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'img/[name][ext]' },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: { filename: 'fonts/[name][ext]' },
+      },
     ],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    }),
+    new BundleTracker({ path: __dirname, filename: 'webpack-stats.json' }),
+    new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
+    new VueLoaderPlugin(),
+    new CompressionPlugin({ test: /\.(js|css|html|svg)$/, algorithm: 'gzip', threshold: 10240, minRatio: 0.8 }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'window.$': 'jquery',
+      Popper: ['@popperjs/core', 'default'],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'node_modules/@popperjs/core/dist/umd/popper.min.js.map'),
+          to: 'core/js',
         },
-        extensions: ['.js', '.vue', '.json'],
-    },
-    optimization: {
-        minimize: true,
-        splitChunks: {
-            chunks: 'all', // Enable code splitting
-            maxSize: 244000, // Split chunks larger than 244 KiB
+        {
+          from: path.resolve(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.min.js.map'),
+          to: 'core/js',
         },
-        minimizer: [
-            new TerserPlugin(),
-            new CssMinimizerPlugin(),
-        ],
-    },
+        {
+          from: path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css.map'),
+          to: 'core/css',
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: { vue: 'vue/dist/vue.esm-bundler.js' },
+    extensions: ['.js', '.vue', '.json'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({ extractComments: false }),
+      new CssMinimizerPlugin(),
+    ],
+  },
 };
