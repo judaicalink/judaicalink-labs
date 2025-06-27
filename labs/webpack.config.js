eeaef35d762640700aee8f6b2078e20cb16caee0
Benjamin Schnabel <Benjamin-777@gmx.de>
2025-06-28 00:54:56 +0200

    webpack updated

--- labs/webpack.config.js
+++ labs/webpack.config.js
@@ -9,35 +9,32 @@ const BundleTracker = require('webpack-bundle-tracker');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  // Only one JS entry that pulls in Bootstrap, Vue apps, and SCSS
   entry: {
-    app: './src/js/app.js',
-    bootstrap: './src/js/bootstrap.js',
-    styles: './src/scss/app.scss',
-    autocomplete: './src/js/autocomplete.js',
+    app: path.resolve(__dirname, 'src/js/app.js'),
   },
+
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'js/[name].js',
+    filename: 'js/app.js',
     publicPath: '/static/',
-    sourceMapFilename: 'js/[file].map',
+    sourceMapFilename: 'js/app.js.map',
   },
+
   mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : 'source-map',
+
   module: {
     rules: [
       { enforce: 'pre', test: /\.js$/, use: ['source-map-loader'] },
       { test: /\.vue$/, loader: 'vue-loader' },
       { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
       {
-        test: /\.s[ac]ss$/i,
+        test: /\.s?css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset/resource',
         generator: { filename: 'img/[name][ext]' },
       },
@@ -48,6 +45,7 @@ module.exports = {
       },
     ],
   },
+
   plugins: [
     new webpack.DefinePlugin({
       __VUE_OPTIONS_API__: true,
@@ -55,7 +53,7 @@ module.exports = {
       __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
     }),
     new BundleTracker({ path: __dirname, filename: 'webpack-stats.json' }),
-    new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
+    new MiniCssExtractPlugin({ filename: 'css/app.css' }),
     new VueLoaderPlugin(),
     new CompressionPlugin({ test: /\.(js|css|html|svg)$/, algorithm: 'gzip', threshold: 10240, minRatio: 0.8 }),
     new webpack.ProvidePlugin({
@@ -69,32 +67,31 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'node_modules/@popperjs/core/dist/umd/popper.min.js.map'),
-          to: 'core/js',
+          to: 'js',
         },
         {
           from: path.resolve(__dirname, 'node_modules/bootstrap/dist/js/bootstrap.min.js.map'),
-          to: 'core/js',
+          to: 'js',
         },
         {
           from: path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.min.css.map'),
-          to: 'core/css',
+          to: 'css',
         },
         {
-          from: path.resolve(__dirname, 'src/img/**/*'),
+          from: path.resolve(__dirname, 'src/img'),
           to: 'img',
         },
       ],
     }),
   ],
+
   resolve: {
     alias: { vue: 'vue/dist/vue.esm-bundler.js' },
     extensions: ['.js', '.vue', '.json'],
   },
+
   optimization: {
     minimize: true,
-    minimizer: [
-      new TerserPlugin({ extractComments: false }),
-      new CssMinimizerPlugin(),
-    ],
+    minimizer: [new TerserPlugin({ extractComments: false }), new CssMinimizerPlugin()],
   },
 };
