50154543b26e9208e1a80cd37c0a858af250b0ba
bsesic <Benjamin-777@gmx.de>
2025-11-20 21:14:01 +0100

    lodjango deleted

--- labs/lodjango/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LodjangoConfig(AppConfig):
-    name = 'lodjango'
--- labs/lodjango/settings.py
+++
@@ -1,6 +0,0 @@
-from django.conf import settings
-
-DATA_PREFIX = 'http://data.judaicalink.org/'
-LOCAL_PREFIX = settings.LABS_ROOT + 'lod/'
-
-SPARQL_ENDPOINT = settings.FUSEKI_SERVER + 'judaicalink/query'
